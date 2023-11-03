1a13b9d506e6c701443a310f5143476e97d1cf98
jayasaptr <jaya.saputra@talenavi.com>
2023-11-03 16:53:57 +0800

    feat:[MJ] instalation cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
+
+app.use(cors());
 
 app.use(express.json());
 
